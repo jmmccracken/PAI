a5bad2bc68e3cdac28a45c65da0843e83e4ae8e6
jmmccracken <jmccrac1@masonlive.gmu.edu>
2014-07-30 19:34:14 -0400

    Fixed the test shell script

--- test/PAItesting.sh
+++ test/PAItesting.sh
@@ -3,7 +3,7 @@
 #Required files for PAI test: PAItesting_A3.00_B2.60.dat & PAItestingAB3_out_PAI3.dat
 
 echo "Testing CCM ..."
-./PAI -E 3 -t 1 -L 2000 -n 1 -Ey 3 -ty 1 -f PAItesting_A3.00_B2.60.dat -o PAItestingAB3_out.test -eY temp_eY.dat
+./../PAI -E 3 -t 1 -L 2000 -n 1 -Ey 3 -ty 1 -f PAItesting_A3.00_B2.60.dat -o PAItestingAB3_out.test -eY temp_eY.dat
 DIFF=$(diff PAItestingAB3_out.dat PAItestingAB3_out.test)
 if [ "$DIFF" = "" ] 
 then
@@ -13,7 +13,7 @@ else
 fi
 
 echo "Testing PAI ..."
-./PAI -E 3 -t 1 -L 2000 -n 1 -Ey 3 -ty 1 -f PAItesting_A3.00_B2.60.dat -o PAItestingAB3_out_PAI3.test -eY PAItestingAB3_eY_PAI3.dat -PAI
+./../PAI -E 3 -t 1 -L 2000 -n 1 -Ey 3 -ty 1 -f PAItesting_A3.00_B2.60.dat -o PAItestingAB3_out_PAI3.test -eY PAItestingAB3_eY_PAI3.dat -PAI
 DIFF=$(diff PAItestingAB3_out_PAI3.dat PAItestingAB3_out_PAI3.test)
 if [ "$DIFF" = "" ] 
 then
