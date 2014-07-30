5c4918feb1c60768541e5372ae2cf579df93c704
jmmccracken <jmccrac1@masonlive.gmu.edu>
2014-07-30 19:51:01 -0400

    Fixed the test shell script to work with the Makefile

--- test/PAItesting.sh
+++ test/PAItesting.sh
@@ -3,8 +3,8 @@
 #Required files for PAI test: PAItesting_A3.00_B2.60.dat & PAItestingAB3_out_PAI3.dat
 
 echo "Testing CCM ..."
-./../PAI -E 3 -t 1 -L 2000 -n 1 -Ey 3 -ty 1 -f PAItesting_A3.00_B2.60.dat -o PAItestingAB3_out.test -eY temp_eY.dat
-DIFF=$(diff PAItestingAB3_out.dat PAItestingAB3_out.test)
+./PAI -E 3 -t 1 -L 2000 -n 1 -Ey 3 -ty 1 -f test/PAItesting_A3.00_B2.60.dat -o test/PAItestingAB3_out.test -eY temp_eY.dat
+DIFF=$(diff test/PAItestingAB3_out.dat test/PAItestingAB3_out.test)
 if [ "$DIFF" = "" ] 
 then
     echo "  --PASSED--"
@@ -13,8 +13,8 @@ else
 fi
 
 echo "Testing PAI ..."
-./../PAI -E 3 -t 1 -L 2000 -n 1 -Ey 3 -ty 1 -f PAItesting_A3.00_B2.60.dat -o PAItestingAB3_out_PAI3.test -eY PAItestingAB3_eY_PAI3.dat -PAI
-DIFF=$(diff PAItestingAB3_out_PAI3.dat PAItestingAB3_out_PAI3.test)
+./PAI -E 3 -t 1 -L 2000 -n 1 -Ey 3 -ty 1 -f test/PAItesting_A3.00_B2.60.dat -o test/PAItestingAB3_out_PAI3.test -eY test/PAItestingAB3_eY_PAI3.dat -PAI
+DIFF=$(diff test/PAItestingAB3_out_PAI3.dat test/PAItestingAB3_out_PAI3.test)
 if [ "$DIFF" = "" ] 
 then
     echo "  --PASSED--"
