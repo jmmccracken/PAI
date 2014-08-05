07567d622f60c3ab0258ec9835c8102b18b2d622
Jim <jmccrac1@masonlive.gmu.edu>
2014-08-05 09:25:47 -0400

    Fixed the directory name and check script

---
+++ check/PAItesting.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#Required files for CCM test: PAItesting_A3.00_B2.60.dat & PAItestingAB3_out.dat
+#Required files for PAI test: PAItesting_A3.00_B2.60.dat & PAItestingAB3_out_PAI3.dat
+n=0
+echo "Testing CCM ..."
+./PAI -E 3 -t 1 -L 2000 -n 1 -Ey 3 -ty 1 -f check/PAItesting_A3.00_B2.60.dat -o check/PAItestingAB3_out.test -eY temp_eY.dat
+DIFF=$(diff check/PAItestingAB3_out.dat check/PAItestingAB3_out.test)
+if [ "$DIFF" = "" ] 
+then
+    echo "  --PASSED--"
+    n=$((n+1))
+else
+    echo "  !! FAILED"
+fi
+
+echo "Testing PAI ..."
+./PAI -E 3 -t 1 -L 2000 -n 1 -Ey 3 -ty 1 -f check/PAItesting_A3.00_B2.60.dat -o check/PAItestingAB3_out_PAI3.test -eY check/PAItestingAB3_eY_PAI3.dat -PAI
+DIFF=$(diff check/PAItestingAB3_out_PAI3.dat check/PAItestingAB3_out_PAI3.test)
+if [ "$DIFF" = "" ] 
+then
+    echo "  --PASSED--"
+    n=$((n+1))
+else
+    echo "  !! FAILED"
+fi
+
+if [ n = 2 ]
+then
+    exit 0
+else
+    exit 1
+fi
